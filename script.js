37fb60c5f0a6abf1b004a2868f6b03ce7b1f8cb0
unknown <jtc7537@gmail.com>
2017-12-23 14:40:30 -0500

    refined countdown timer and confirm button

--- script.js
+++ script.js
@@ -15,28 +15,75 @@ var triviaGame = {
 
 	correctAnswerCount: 0,
 	questionCounter: 0,
+	remainingSeconds: 20,
+	confirmCounter: 0,
 
 	questionArray: ["<h1>1: What is the answer to this question?</h1><div id='wrong-answer' class='answers'>Wrong answer</div><div id='correct-answer' class='answers'>Correct answer</div>", "<h1>2: What is the answer to this question?</h1><div id='wrong-answer' class='answers'>Wrong answer</div><div id='correct-answer' class='answers'>Correct answer</div>","<h1>3: What is the answer to this question?</h1><div id='wrong-answer' class='answers'>Wrong answer</div><div id='correct-answer' class='answers'>Correct answer</div>"],
 
-	correctAnswer: ["<h1>The corrct anser was:</h1><div>Correct answer</div>"]
+	correctAnswer: ["<h1>The correct answer was:</h1><div>Correct answer</div>"]
 
 }
 
+function deprecateTimer(){
+
+	triviaGame.remainingSeconds--;
+	$("#remaining-seconds").text(triviaGame.remainingSeconds)
+}
+
+function timerTrigger(){
+	if(triviaGame.questionCounter > 2){
+		return;
+	}
+	timer = setInterval(deprecateTimer, 1000)
+}
+
+
+var timer;
 
 function nextQuestion(){
+
+	triviaGame.confirmCounter = 0;
+
+	if(triviaGame.questionCounter === triviaGame.questionArray.length){
+		$("#questions").html("<h1>Final Score</h1><h2>Correct Answers : <span id='correct-answers'></span></h2><h2>Wrong Answers: <span id='wrong-answers'></span></h2>");
+		$("#correct-answers").text(triviaGame.correctAnswerCount);
+		$("#rem-sec-holder").empty();
+		return;
+	}
+	triviaGame.remainingSeconds = 20;
+	clearInterval(timer);
+	timerTrigger();
+
 	$("#questions").html(triviaGame.questionArray[triviaGame.questionCounter])
-	newQuestionTimer = setTimeout(nextQuestion, 10000);
+	newQuestionTimer = setTimeout(nextQuestion, 20000);
+
 	triviaGame.questionCounter++;
 
 	$("#correct-answer").click(function(){
-		alert("Correct answer chosen");
 		triviaGame.correctAnswerCount++;
+		console.log(triviaGame.correctAnswerCount)
 	});
 
 }
 
+$(".answers").click(function(){
+	$(this).removeClass("answers").addClass("chosen-answer")
+	console.log("change answer class")
+})
+
+$(".chosen-answer").click(function(){
+	$(this).removeClass("chosen-answer").addClass("answers")
+})
+
+
 
 $("#confirm-answer").click(function(){
+	if(triviaGame.confirmCounter === 1){
+		clearTimeout(confirmNext);
+		nextQuestion();
+		return;
+	}
+	triviaGame.confirmCounter++;
 	clearTimeout(newQuestionTimer);
 	$("#questions").html(triviaGame.correctAnswer[0])
 	var confirmNext = setTimeout(nextQuestion, 1500);
